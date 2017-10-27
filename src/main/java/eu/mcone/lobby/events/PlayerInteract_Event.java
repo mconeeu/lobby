ed9ede6b07d72f5cd0868cd86572e91974104d3a
Rufus Maiwald <rufusmaiwald@outlook.de>
2017-10-27 20:17:19 +0200

    PlayerInteract_Event throws NullPointerException Error Fix

--- src/main/java/eu/mcone/lobby/events/PlayerInteract_Event.java
+++ src/main/java/eu/mcone/lobby/events/PlayerInteract_Event.java
@@ -13,6 +13,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class PlayerInteract_Event implements Listener{
 
@@ -20,22 +21,21 @@ public class PlayerInteract_Event implements Listener{
     public void onInteract(PlayerInteractEvent e){
         Player p = e.getPlayer();
 
-        try{
-            if (((e.getAction() == Action.RIGHT_CLICK_AIR) || (e.getAction() == Action.RIGHT_CLICK_BLOCK))) {
-                if (e.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§3§lProfil §8» §7§oAussehen / Effekte / Gadgets")) {
-                    new Profil_Interact(p);
-                } else if (p.getItemInHand().getItemMeta().getDisplayName().equals("§3§lSpieler Verstecken §8» §7§oBlende alle anderen Spieler aus")) {
-                    Hider_Interact.hidePlayers(p);
-                } else if (p.getItemInHand().getItemMeta().getDisplayName().equals("§3§lSpieler Anzeigen §8» §7§oZeigt alle Spieler wieder an")) {
-                    Hider_Interact.showPlayers(p);
-                } else if (p.getItemInHand().getItemMeta().getDisplayName().equals("§3§lStats §8» §7§oStatistiken zu allen Spielmodi")) {
-                    p.performCommand("stats");
-                } else if (e.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§3§lNavigator §8» §7§oWähle einen Spielmodus")) {
-                    new Kompass_Interact(e, p);
-                }
+        ItemStack i = p.getItemInHand();
+        if ((i == null) || (!i.hasItemMeta()) || (!i.getItemMeta().hasDisplayName())) {
+            return;
+        } else if ((e.getAction() == Action.RIGHT_CLICK_AIR) || (e.getAction() == Action.RIGHT_CLICK_BLOCK)) {
+            if (e.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§3§lProfil §8» §7§oAussehen / Effekte / Gadgets")) {
+                new Profil_Interact(p);
+            } else if (p.getItemInHand().getItemMeta().getDisplayName().equals("§3§lSpieler Verstecken §8» §7§oBlende alle anderen Spieler aus")) {
+                Hider_Interact.hidePlayers(p);
+            } else if (p.getItemInHand().getItemMeta().getDisplayName().equals("§3§lSpieler Anzeigen §8» §7§oZeigt alle Spieler wieder an")) {
+                Hider_Interact.showPlayers(p);
+            } else if (p.getItemInHand().getItemMeta().getDisplayName().equals("§3§lStats §8» §7§oStatistiken zu allen Spielmodi")) {
+                p.performCommand("stats");
+            } else if (e.getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§3§lNavigator §8» §7§oWähle einen Spielmodus")) {
+                new Kompass_Interact(e, p);
             }
-        }catch(Exception e1){
-            e1.printStackTrace();
         }
     }
 }
